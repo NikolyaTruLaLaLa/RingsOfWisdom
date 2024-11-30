9827e7f6e9b0e56b4c72e19a622b28998c61fbc6
Николай Игнатенко <146518175+NikolyaTruLaLaLa@users.noreply.github.com>
2024-11-30 17:22:05 +0300

    Add files via upload

---
+++ backend/mabyWorking/DAL/ROWContext.cs
@@ -0,0 +1,33 @@
+﻿using mabyWorking.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace mabyWorking.DAL
+{
+    public class ROWContext : DbContext
+    {
+        public ROWContext(DbContextOptions<ROWContext> options) : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Progress_in_skills> Progresses_In_Skills { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<Direction> Directions { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Solved_question> SolvedQuestions { get; set; }
+        public DbSet<Area> Area { get; set; }
+        public DbSet<User_expirience_in_areas> User_expirience_in_area { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<Progress_in_skills>().ToTable("Progress_in_skills");
+            modelBuilder.Entity<Skill>().ToTable("Skill");
+            modelBuilder.Entity<Direction>().ToTable("Direction");
+            modelBuilder.Entity<Question>().ToTable("Question");
+            modelBuilder.Entity<Solved_question>().ToTable("Question");
+            modelBuilder.Entity<Area>().ToTable("Area");
+            modelBuilder.Entity<User_expirience_in_areas>().ToTable("User_expirience_in_area");
+
+        }
+    }
+}
---
+++ backend/mabyWorking/Models/User_expirience_in_areas.cs
@@ -0,0 +1,13 @@
+﻿namespace mabyWorking.Models
+{
+    public class User_expirience_in_areas
+    {
+        public int id { get; set; }
+        public int areaID { get; set; }
+        public int userID { get; set; }
+        public int experience { get; set; }
+
+        public Area Area { get; set; }
+        public User User { get; set; }
+    }
+}
