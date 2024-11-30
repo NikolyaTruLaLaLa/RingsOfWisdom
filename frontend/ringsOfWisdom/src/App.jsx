522529a97506db276f6b50a147514386277f9ef5
Kombat712 <sasanovikov712@gmail.com>
2024-12-01 00:03:37 +0300

    added main default page

--- frontend/ringsOfWisdom/src/App.jsx
+++ frontend/ringsOfWisdom/src/App.jsx
@@ -1,33 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import "./assets/style/style_main_page.css";
+import logo from"./assets/images/Logo Black.png";
+import profile from"./assets/images/profile-icon.png";
 import './App.css'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <header className="site-header">
+        <div className="logo">
+            <a href="#logo">
+            <img src={logo} alt="Логотип" className="logo"/>
+            </a>
+        </div>
+        <nav className="nav-links">
+            <a href="#courses">Курсы</a>
+            <a href="#skills">Дерево навыков</a>
+            <a href="#store">Магазин</a>
+        </nav>
+        <div className="profile">
+            <a href="#profile">
+            <img src={profile} alt="Иконка профиля" />
+            </a>
+        </div>
+      </header>
+    
+      <div className="heading-container">
+          <h1>Main Page</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="main-content">
+          <h1>Work in Progress</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
