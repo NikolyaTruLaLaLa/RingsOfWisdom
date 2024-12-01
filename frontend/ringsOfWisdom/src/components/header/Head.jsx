5f3b6fb57882746d4d6621098f0d7d03668defe9
Kombat712 <sasanovikov712@gmail.com>
2024-12-01 13:14:56 +0300

    added nav for head

--- frontend/ringsOfWisdom/src/components/header/Head.jsx
+++ frontend/ringsOfWisdom/src/components/header/Head.jsx
@@ -1,26 +1,27 @@
 import logo from"./../../assets/images/Logo Black.png";
 import profile from"./../../assets/images/profile-icon.png";
 
+import { NavLink } from 'react-router-dom';
+import React from 'react';
+
 import "./stylehead.css"
 
 const Head = () => {
     return ( 
     <header className="site-header">
-        <div className="logo">
-            <a href="/main">
-            <img src={logo} alt="Логотип" className="logo"/>
-            </a>
-        </div>
+        <NavLink to="/main" className="logo">
+            
+            <img src={logo} alt="Логотип" />
+            
+        </NavLink>
         <nav className="nav-links">
-            <a href="/course">Курсы</a>
-            <a href="/skills">Дерево навыков</a>
-            <a href="/shop">Магазин</a>
+            <NavLink to="/course">Курсы</NavLink>
+            <NavLink to="/skills">Дерево навыков</NavLink>
+            <NavLink to="/shop">Магазин</NavLink>
         </nav>
-        <div className="profile">
-            <a href="/auth">
-            <img src={profile} alt="Иконка профиля" />
-            </a>
-        </div>
+        <NavLink to="/auth" className="profile">
+            <img src={profile} alt="Иконка профиля" />    
+        </NavLink>
     </header> 
     );
 }
