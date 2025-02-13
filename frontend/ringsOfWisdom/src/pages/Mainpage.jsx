133551ff3075bcea505a736e3b92a52e63182cb6
Kombat712 <sasanovikov712@gmail.com>
2025-02-13 19:34:01 +0300

    Update Mainpage.jsx

--- frontend/ringsOfWisdom/src/pages/Mainpage.jsx
+++ frontend/ringsOfWisdom/src/pages/Mainpage.jsx
@@ -1,9 +1,14 @@
+<<<<<<< HEAD
 import logom from"./../assets/images/logo.png";
 import Footer from './../components/footer/Footer';
 
+=======
+>>>>>>> ecb6bb7d3f2986624d312b0b4b1583d5c9a7daa1
 import React from 'react';
+import logom from "./../assets/images/logo.png";
 
 const Mainpage = () => {
+<<<<<<< HEAD
     
     return ( 
     <>
@@ -18,4 +23,26 @@ const Mainpage = () => {
      );
 }
  
+=======
+    return (
+        <div className="container">
+            <img src={logom} alt="Логотип Rings of Wisdom" className="logom" />
+            
+            <div className="text-container">
+                <p className="description">
+                    Вебсервис на ASP.NET core версии 4.8 для обучения основам игры в ЧГК. По механикам за основу взят GO MAGIC.
+                </p>
+                
+                <div className="goal-container">
+                    <h2>Цель</h2>
+                    <p className="goal-text">
+                        Предоставить образовательную программу по игре ЧГК в удобной форме древа навыков.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+>>>>>>> ecb6bb7d3f2986624d312b0b4b1583d5c9a7daa1
 export default Mainpage;
