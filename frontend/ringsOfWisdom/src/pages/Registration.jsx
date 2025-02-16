65604050078788b45f2ad235c0d2945c3d27341d
Kombat712 <sasanovikov712@gmail.com>
2025-02-16 19:06:01 +0300

    сделал фронт регистрацию и написал хук под неё

--- frontend/ringsOfWisdom/src/pages/Registration.jsx
+++ frontend/ringsOfWisdom/src/pages/Registration.jsx
@@ -1,25 +1,53 @@
 import logom from"./../assets/images/logo.png";
 import './../assets/style/style_registration.css';
+import { useState } from "react";
+import axios from "axios";
 
 import { NavLink } from 'react-router-dom';
 import React from 'react';
 
-const Registation = () => {
+function  Registation() {
+    const [formData, setFormData] = useState({
+      userName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
+  
+    const [message, setMessage] = useState("");
+  
+    const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      setMessage("");
+  
+      try {
+        const response = await axios.post("http://localhost:5000/api/register", formData);
+        setMessage(response.data.message);
+      } catch (error) {
+        setMessage("Ошибка: " + (error.response?.data?.[0]?.description || "Неизвестная ошибка"));
+      }
+    };
+
     return ( 
     <div className="container">
         
         <img src={logom} alt="Логотип Rings of Wisdom" className="logom"/>
+        {message && <p>{message}</p>}
         
         <div className="registration-box">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Регистрация игрока</label>
                 <NavLink to="/auth" className="enter-account">Войти</NavLink>
                 
                 
-                <input type="email" placeholder="Email" required/>
-                <input type="password" placeholder="Пароль" required/>
-                <input type="password" placeholder="Повтори Пароль" required/>
-                <input type="name" placeholder="Имя пользователя" required/>
+                <input type="email" name="email" placeholder="Email" onChange={handleChange} required/>
+                <input type="password" name="password" placeholder="Password" onChange={handleChange} required/>
+                <input type="password" name="confirmPassword" placeholder="Confirm Password" onChange={handleChange} required/>
+                <input type="text" name="userName" placeholder="UserName" onChange={handleChange} required/>
                   
                 
                 <NavLink to="/emlverif">
