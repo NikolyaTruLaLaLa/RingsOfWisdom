d641caabdd14b8eae3f0c3ad6d84e3bca070d05d
Захар <fatalitydo@gmail.com>
2025-02-25 22:25:36 +0300

    поменял названия классов в jsx

--- frontend/ringsOfWisdom/src/pages/Mainpage.jsx
+++ frontend/ringsOfWisdom/src/pages/Mainpage.jsx
@@ -6,17 +6,17 @@ import logom from "./../assets/images/logo.png";
 const Mainpage = () => {
     return (
         <>
-        <div className="container">
+        <div className="container_main">
             <img src={logom} alt="Логотип Rings of Wisdom" className="logom" />
             
-            <div className="text-container">
-                <p className="description">
+            <div className="text-container_main">
+                <p className="description_main">
                     Вебсервис на ASP.NET core версии 4.8 для обучения основам игры в ЧГК. По механикам за основу взят GO MAGIC.
                 </p>
                 
-                <div className="goal-container">
+                <div className="goal-container_main">
                     <h2>Цель</h2>
-                    <p className="goal-text">
+                    <p className="goal-text_main">
                         Предоставить образовательную программу по игре ЧГК в удобной форме древа навыков.
                     </p>
                 </div>
