e4d45f19c6f7e30e2b9d4ae0058a646bb8e68670
ramrost6 <ramrost666@gmail.com>
2025-03-18 21:46:15 +0300

    Добавлен лимит квизов

--- frontend/ringsOfWisdom/src/pages/Shop.jsx
+++ frontend/ringsOfWisdom/src/pages/Shop.jsx
@@ -2,50 +2,27 @@ import './../assets/style/style_shop.css';
 
 const Shop = () => {
     const balance = 0; 
-    const totalQuizzes = 100;
-    const remainingQuizzes = 75; 
+    const availableQuizzes = 5; 
+    const totalQuizzes = 10; 
 
     return (
         <div className="shop-container">
             <div className="header-info">
-                <h1>ROW Store</h1>
+                <h1>Go Magic Store</h1>
                 <div className="balance-info">
                     <span className="coin-icon">🪙</span>
                     <span>{balance}</span>
+                    <span className="quiz-counter">{availableQuizzes}/{totalQuizzes} Осталось квизов</span>
                 </div>
             </div>
 
             <div className="purchase-options">
                 <div className="purchase-item">
                     <div className="icon-circle">
-                        <span>🚫</span> 
+                        <span>📈</span> 
                     </div>
-                    <p className="item-description">Disable Ads for 30 Days</p>
-                    <button className="purchase-button">150</button>
-                </div>
-
-                <div className="purchase-item">
-                    <div className="icon-circle">
-                        <span>🌟</span> 
-                    </div>
-                    <p className="item-description">No Ads Ever Again</p>
-                    <button className="purchase-button">1500</button>
-                </div>
-
-                <div className="purchase-item">
-                    <div className="icon-circle">
-                        <span>🎓</span> 
-                    </div>
-                    <p className="item-description">Discount on a Membership</p>
-                    <button className="purchase-button">150</button>
-                </div>
-
-                <div className="purchase-item">
-                    <div className="icon-circle">
-                        <span>📚</span> 
-                    </div>
-                    <p className="item-description">Discount on a Course</p>
-                    <button className="purchase-button">100</button>
+                    <p className="item-description">Увеличить дневной лимит квизов</p>
+                    <button className="purchase-button">200</button>
                 </div>
             </div>
         </div>
