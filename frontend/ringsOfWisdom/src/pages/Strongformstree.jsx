f78d50c79b9936e46fbd20ec1817664cd30e868b
Kombat712 <sasanovikov712@gmail.com>
2025-03-20 01:03:36 +0300

    изменил стили всплывающего окошка и профиля

--- frontend/ringsOfWisdom/src/pages/Strongformstree.jsx
+++ frontend/ringsOfWisdom/src/pages/Strongformstree.jsx
@@ -43,46 +43,62 @@ const Strongformstree = () => {
 
   return (
     <div className="container_all">
-      <div className="head">
-        <p>Могучие Формы</p>
-        {/*<span className="quiz-counter">{availableQuizzes}/{totalQuizzes} Осталось квизов</span>*/}
-      </div>
-      <div className="container_first">
-        <NavLink to={quizData[0].path} className="item">
-          <img src={quizData[0].image} alt={quizData[0].name} />
-          <p>{quizData[0].name}</p>
-        </NavLink>
-      </div>
-      <div className="container_second">
-        <NavLink to={quizData[1].path} className="item">
-          <img src={quizData[1].image} alt={quizData[1].name} />
-          <p>{quizData[1].name}</p>
-        </NavLink>
-        <NavLink to={quizData[2].path} className="item">
-          <img src={quizData[2].image} alt={quizData[2].name} />
-          <p>{quizData[2].name}</p>
-        </NavLink>
-      </div>
-      <div className="container_last">
-        <NavLink to={quizData[3].path} className="item">
-          <img src={quizData[3].image} alt={quizData[3].name} />
-          <p>{quizData[3].name}</p>
-        </NavLink>
-      </div>
+  <div className="head">
+    <p>Могучие Формы</p>
+    {/*<span className="quiz-counter">{availableQuizzes}/{totalQuizzes} Осталось квизов</span>*/}
+  </div>
 
-      {isPopupVisible && (
-        <div className="popupwin">
-          <div className="popupwin-content">
-            <span className="close-btn" onClick={handleClosePopup}>
-              &times;
-            </span>
-            <p>
-              При переходе на квиз, считывается попытка квиза в день. Пройденным - квиз считается после взятия первых 2-ух вопросов.
-            </p>
-          </div>
-        </div>
-      )}
+  {/* Для больших экранов (>= 1025px) */}
+  {window.innerWidth > 1025 && isPopupVisible && (
+    <div className="popupwin">
+      <div className="popupwin-content">
+        <span className="close-btn" onClick={handleClosePopup}>
+          &times;
+        </span>
+        <p>
+          При переходе на квиз, считывается попытка квиза в день. Пройденным - квиз считается после взятия первых 2-ух вопросов.
+        </p>
+      </div>
     </div>
+  )}
+
+  <div className="container_first">
+    <NavLink to={quizData[0].path} className="item">
+      <img src={quizData[0].image} alt={quizData[0].name} />
+      <p>{quizData[0].name}</p>
+    </NavLink>
+  </div>
+  <div className="container_second">
+    <NavLink to={quizData[1].path} className="item">
+      <img src={quizData[1].image} alt={quizData[1].name} />
+      <p>{quizData[1].name}</p>
+    </NavLink>
+    <NavLink to={quizData[2].path} className="item">
+      <img src={quizData[2].image} alt={quizData[2].name} />
+      <p>{quizData[2].name}</p>
+    </NavLink>
+  </div>
+  <div className="container_last">
+    <NavLink to={quizData[3].path} className="item">
+      <img src={quizData[3].image} alt={quizData[3].name} />
+      <p>{quizData[3].name}</p>
+    </NavLink>
+  </div>
+
+  {/* Для экранов меньше 1025px */}
+  {window.innerWidth <= 1025 && isPopupVisible && (
+    <div className="popupwin">
+      <div className="popupwin-content">
+        <span className="close-btn" onClick={handleClosePopup}>
+          &times;
+        </span>
+        <p>
+          При переходе на квиз, считывается попытка квиза в день. Пройденным - квиз считается после взятия первых 2-ух вопросов.
+        </p>
+      </div>
+    </div>
+  )}
+</div>
   );
 };
 
