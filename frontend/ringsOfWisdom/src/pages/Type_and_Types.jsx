92c905a46c60837c2713ed292a1f9b4a55ab0582
softc0de <fatalitydo@gmail.com>
2025-03-19 20:50:19 +0300

    сделал кружки кликабельными

--- frontend/ringsOfWisdom/src/pages/Type_and_Types.jsx
+++ frontend/ringsOfWisdom/src/pages/Type_and_Types.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 import './../assets/style/type_and_types.css';
 import baza from './../assets/images/baza.png';
 import razdatka from './../assets/images/razdatka.png';
@@ -15,93 +17,93 @@ const Type_and_Types = () => {
     
     return ( 
         <div className="skill-tree">
-            <p className="page-title">Типы типочки</p>
-            
-            {/* Первый ряд (1 кружок) */}
-            <div className="skill-row">
-                <div className="circle-container">
-                    <div className="circle">
-                        <img src={baza} alt="БАЗА" />
-                    </div>
-                    <p>БАЗА</p>
-                </div>
-            </div>
+        {/* Заголовок страницы */}
+        <p className="page-title">Типы типочки</p>
 
-            {/* Второй ряд (2 кружка) */}
-            <div className="skill-row">
-                <div className="circle-container">
-                    <div className="circle">
-                        <img src={razdatka} alt="Раздатка" />
-                    </div>
-                    <p>Раздатка</p>
+        {/* Первый ряд (1 кружок) */}
+        <div className="skill-row">
+            <NavLink to="/skillwp" className="circle-container"> {/* Кликабельный кружок */}
+                <div className="circle">
+                    <img src={baza} alt="БАЗА" />
                 </div>
-                <div className="circle-container">
-                    <div className="circle">
-                        <img src={zamena} alt="Замена" />
-                    </div>
-                    <p>Замена</p>
-                </div>
-            </div>
-
-            {/* Третий ряд (2 кружка) */}
-            <div className="skill-row">
-                <div className="circle-container">
-                    <div className="circle">
-                        <img src={blatie_nomera} alt="Блатные номера" />
-                    </div>
-                    <p>Блатные<br/>номера</p>
-                </div>
-                <div className="circle-container">
-                    <div className="circle">
-                        <img src={razdatOCHKA} alt="РаздатОЧКА" />
-                    </div>
-                    <p>РаздатОЧКА</p>
-                </div>
-            </div>
-
-            {/* Четвертый ряд (2 кружка) */}
-            <div className="skill-row">
-                <div className="circle-container">
-                    <div className="circle">
-                        <img src={zamena_zamen} alt="Замена замен замены замен" />
-                    </div>
-                    <p>Замена замен<br/>замены замен</p>
-                </div>
-                <div className="circle-container">
-                    <div className="circle">
-                        <img src={svyazannye} alt="Связанные одной цепью" />
-                    </div>
-                    <p>Связанные<br/>одной цепью</p>
-                </div>
-            </div>
-
-            {/* Пятый ряд (2 кружка) */}
-            <div className="skill-row">
-                <div className="circle-container">
-                    <div className="circle">
-                        <img src={kot} alt="Кот в Чёрном Ящике" />
-                    </div>
-                    <p>Кот в Чёрном<br/>Ящике</p>
-                </div>
-                <div className="circle-container">
-                    <div className="circle">
-                        <img src={frazochki} alt="Странные фразочки" />
-                    </div>
-                    <p>Странные<br/>фразочки</p>
-                </div>
-            </div>
-
-            {/* Шестой ряд (1 кружок) */}
-            <div className="skill-row">
-                <div className="circle-container">
-                    <div className="circle">
-                        <img src={blitz} alt="БЛИЦ БЛИЦ БЛИЦ" />
-                    </div>
-                    <p>БЛИЦ БЛИЦ<br/>БЛИЦ</p>
-                </div>
-            </div>
+                <p>БАЗА</p>
+            </NavLink>
         </div>
-        
+
+        {/* Второй ряд (2 кружка) */}
+        <div className="skill-row">
+            <NavLink to="/skillwp" className="circle-container"> {/* Кликабельный кружок */}
+                <div className="circle">
+                    <img src={razdatka} alt="Раздатка" />
+                </div>
+                <p>Раздатка</p>
+            </NavLink>
+            <NavLink to="/skillwp" className="circle-container"> {/* Кликабельный кружок */}
+                <div className="circle">
+                    <img src={zamena} alt="Замена" />
+                </div>
+                <p>Замена</p>
+            </NavLink>
+        </div>
+
+        {/* Третий ряд (2 кружка) */}
+        <div className="skill-row">
+            <NavLink to="/skillwp" className="circle-container"> {/* Кликабельный кружок */}
+                <div className="circle">
+                    <img src={blatie_nomera} alt="Блатные номера" />
+                </div>
+                <p>Блатные<br/>номера</p>
+            </NavLink>
+            <NavLink to="/skillwp" className="circle-container"> {/* Кликабельный кружок */}
+                <div className="circle">
+                    <img src={razdatOCHKA} alt="РаздатОЧКА" />
+                </div>
+                <p>РаздатОЧКА</p>
+            </NavLink>
+        </div>
+
+        {/* Четвертый ряд (2 кружка) */}
+        <div className="skill-row">
+            <NavLink to="/skillwp" className="circle-container"> {/* Кликабельный кружок */}
+                <div className="circle">
+                    <img src={zamena_zamen} alt="Замена замен замены замен" />
+                </div>
+                <p>Замена замен<br/>замены замен</p>
+            </NavLink>
+            <NavLink to="/skillwp" className="circle-container"> {/* Кликабельный кружок */}
+                <div className="circle">
+                    <img src={svyazannye} alt="Связанные одной цепью" />
+                </div>
+                <p>Связанные<br/>одной цепью</p>
+            </NavLink>
+        </div>
+
+        {/* Пятый ряд (2 кружка) */}
+        <div className="skill-row">
+            <NavLink to="/skillwp" className="circle-container"> {/* Кликабельный кружок */}
+                <div className="circle">
+                    <img src={kot} alt="Кот в Чёрном Ящике" />
+                </div>
+                <p>Кот в Чёрном<br/>Ящике</p>
+            </NavLink>
+            <NavLink to="/skillwp" className="circle-container"> {/* Кликабельный кружок */}
+                <div className="circle">
+                    <img src={frazochki} alt="Странные фразочки" />
+                </div>
+                <p>Странные<br/>фразочки</p>
+            </NavLink>
+        </div>
+
+        {/* Шестой ряд (1 кружок) */}
+        <div className="skill-row">
+            <NavLink to="/skillwp" className="circle-container"> {/* Кликабельный кружок */}
+                <div className="circle">
+                    <img src={blitz} alt="БЛИЦ БЛИЦ БЛИЦ" />
+                </div>
+                <p>БЛИЦ<br/>БЛИЦ БЛИЦ</p>
+            </NavLink>
+        </div>
+    </div>
     );
 };
  
