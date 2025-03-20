0a7732c80b931b50d41d6da88f2856b845644215
Kombat712 <sasanovikov712@gmail.com>
2025-03-20 23:25:25 +0300

    Привёл страничку курсов в работающие состояние( надо в бд курсами пометить все заголовки для более красивой их вёрстки)

--- frontend/ringsOfWisdom/src/pages/Courses.jsx
+++ frontend/ringsOfWisdom/src/pages/Courses.jsx
@@ -1,15 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import "./../assets/style/style_courses.css";
+const API_BASE_URL = "https://localhost:5269/api"; 
 
 const Courses = () => {
     const [courses, setCourses] = useState([]);
     const [selectedCourse, setSelectedCourse] = useState(null);
 
     useEffect(() => {
-        fetch('https://your-backend-api.com/courses')
-            .then(response => response.json())
-            .then(data => setCourses(data))
-            .catch(error => console.error('Error fetching courses:', error));
+        const fetchCourses = async () => {
+            try {
+              const response = await fetch(`${API_BASE_URL}/Course/getCourses`, {
+                method: "GET",
+                headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+                credentials: "include",
+              });
+              const data = await response.json();
+              setCourses(data)
+            } catch (error) {
+              console.error("Ошибка загрузки курсов:", error);
+            }
+          };
+
+          fetchCourses();
     }, []);
 
     const handleCourseClick = (course) => {
@@ -30,9 +42,9 @@ const Courses = () => {
             {selectedCourse && (
                 <div className="modal-overlay">
                     <div className="modal-content">
-                        <h2>{selectedCourse.name}</h2>
-                        <p>{selectedCourse.description}</p>
-                        <a href={selectedCourse.source} target="_blank" rel="noopener noreferrer">Источник</a>
+                        <h2 className="curses-head">{selectedCourse.name}</h2>
+                        <p className="curses-text">{selectedCourse.text}</p>
+                        <a href={selectedCourse.link} target="_blank" rel="noopener noreferrer">Источник</a>
                         <button onClick={closeModal}>Закрыть</button>
                     </div>
                 </div>
